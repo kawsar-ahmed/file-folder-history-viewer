2cdb1f1ba327688a9987739359590c84bd85b167
Kawsar Ahmed <kawsar@divine-it.net>
2015-03-30 18:43:08 +0600

    + User Interface

---
+++ src/ui/MainUI.java
@@ -0,0 +1,84 @@
+package ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+
+import windowsevent.EventCollector;
+
+public class MainUI {
+
+	private JFrame frame;
+	private JTextPane textPane;
+	private JButton refreshButton;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainUI window = new MainUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainUI() {
+		initialize();
+		loadEventData(textPane);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+		textPane = new JTextPane();
+		scrollPane.setViewportView(textPane);
+		
+		refreshButton = new JButton("Refresh");
+		frame.getContentPane().add(refreshButton, BorderLayout.BEFORE_FIRST_LINE);
+		refreshButton.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				loadEventData(textPane);
+			}
+		});
+		
+	}
+
+	private void loadEventData(final JTextPane textPane) {
+		EventQueue.invokeLater(new Runnable() {
+			
+			public void run() {
+				// TODO Auto-generated method stub
+				
+				EventCollector eventCollector = new EventCollector();
+				String eventsText = eventCollector.getEvents();
+				textPane.setText(eventsText);
+			}
+		});
+	}
+
+}
---
+++ src/windowsevent/EventCollector.java
@@ -0,0 +1,41 @@
+package windowsevent;
+
+import com.sun.jna.platform.win32.Advapi32Util.EventLogIterator;
+import com.sun.jna.platform.win32.Advapi32Util.EventLogRecord;
+
+/**
+ * @author Kawsar Ahmed
+ *
+ */
+public class EventCollector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EventLogIterator iter = new EventLogIterator("Secuirity");
+		System.out.println("Start:");
+		while(iter.hasNext()) {
+		    EventLogRecord record = iter.next();
+		    System.out.println(record.getRecordNumber()
+		            + ": Event ID: " + record.getEventId()
+		            + ", Event Type: " + record.getType()
+		            + ", Event Source: " + record.getSource());
+		}
+	}
+	
+	public String getEvents() {
+		EventLogIterator eventLogIterator = new EventLogIterator("Secuirity");
+		String eventsText = "";
+		int i=1000;
+		while(eventLogIterator.hasNext() && i-- > 0) {
+			EventLogRecord eventLogRecord = eventLogIterator.next();
+			eventsText += "\n\r"+eventLogRecord.getRecordNumber()
+		            + ": Event ID: " + eventLogRecord.getEventId()
+		            + ", Event Type: " + eventLogRecord.getType()
+		            + ", Event Source: " + eventLogRecord.getSource();
+		}
+		eventLogIterator.close();
+		return eventsText;
+		
+	}
+
+}
